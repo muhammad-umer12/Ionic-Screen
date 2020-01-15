3de4bc667f64e5d28632ccc7b0e8b737c17294b4
Muhammad Umer <muhammad.umer121212@gmail.com>
2020-01-16 03:09:19 +0500

    first commit

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+
+  constructor() {}
+  
+    gymm=true;
+
+    
+
+    arr=[
+          {gymName: "Gym name Max line. 1",gymDetails:"17:00 ~ 20:30"},
+          {gymName: "Gym name Max line. 1",gymDetails:"17:00 ~ 20:30"},
+          {gymName: "Gym name Max line. 1",gymDetails:"17:00 ~ 20:30"},
+          {gymName: "Gym name Max line. 1",gymDetails:"17:00 ~ 20:30"},
+          {gymName: "Gym name Max line. 1",gymDetails:"17:00 ~ 20:30"},
+  
+        ]
+
+}
